987582363ce6c13ef777945793690946aa63b6e1
geoffreyVanaer <geoffrey.vanaerdewegh@laposte.net>
2017-10-03 15:16:24 +0200

    commit ajout currentDateString

--- Webapps GFI/www/js/createNeed.js
+++ Webapps GFI/www/js/createNeed.js
@@ -4,4 +4,10 @@
  * and open the template in the editor.
  */
 
+var currentDateLong = new Date(); //Récupération de la date courrante
+var annee   = currentDateLong.getFullYear(); 
+var mois    = currentDateLong.getMonth() + 1;
+var jour    = currentDateLong.getDate();
+var currentDate = jour+"/"+mois+"/"+annee; 
+document.getElementById('currentDateString').innerHTML = currentDate;
 
